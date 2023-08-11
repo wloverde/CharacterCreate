8acb73d057a664f917a09e15d1e34140b4daa27f
unknown <peter.ostertag99@gmail.com>
2023-08-10 21:24:14 -0500

    Updated character & index tables + created class table

---
+++ models/Class.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Class extends model {}
+
+Class.inint(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING, // VARCHAR
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'class',
+    }
+);
+
+module.exports = Class;
