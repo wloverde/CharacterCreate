bede8640da242294928f62dd24c6520dd06611c0
Elaine Martinez <elainefmartinez6@gmail.com>
2023-08-15 16:22:30 -0700

    updated userprofile

--- seeds/character/classData.js
+++ seeds/character/classData.js
@@ -10,45 +10,69 @@ const classData =
   },
   {
     "name": "Bard",
-    "ability1" : "swing axe",
-    "ability2" : "get mad",
-    "ability3" : "bonus attack"
+    "ability1" : "spellcasting",
+    "ability2" : "song of rest",
+    "ability3" : "countercharm"
   },
   {
     "name": "Cleric",
-    "ability1" : "swing axe",
-    "ability2" : "get mad",
-    "ability3" : "bonus attack"
+    "ability1" : "spellcasting",
+    "ability2" : "Divine Domain",
+    "ability3" : "channel Divinity"
   },
   {
     "name": "Druid",
-    "ability1" : "swing axe",
-    "ability2" : "get mad",
-    "ability3" : "bonus attack"
+    "ability1" : "wild shape",
+    "ability2" : "druid circle",
+    "ability3" : "circle of the moon"
   },
   {
-    "name": "Fighter"
+    "name": "Fighter",
+    "ability1" : "second wind",
+    "ability2" : "action surge",
+    "ability3" : "indomitable"
   },
   {
-    "name": "Monk"
+    "name": "Monk",
+    "ability1" : "monastic tradition",
+    "ability2" : "slow fall",
+    "ability3" : "evasion"
   },
   {
-    "name": "Paladin"
+    "name": "Paladin",
+    "ability1" : "divine smite",
+    "ability2" : "sacred oath",
+    "ability3" : "spellcasting"
   },
   {
-    "name": "Ranger"
+    "name": "Ranger",
+    "ability1" : "vanish",
+    "ability2" : "lands stride",
+    "ability3" : "primeval awareness"
   },
   {
-    "name": "Rogue"
+    "name": "Rogue",
+    "ability1" : "uncanny dodge",
+    "ability2" : "slippery mind",
+    "ability3" : "cunning action"
   },
   {
-    "name": "Sorcerer"
+    "name": "Sorcerer",
+    "ability1" : "spellcasting",
+    "ability2" : "wild magic",
+    "ability3" : "metamagic"
   },
   {
-    "name": "Warlock"
+    "name": "Warlock",
+    "ability1" : "pact magic",
+    "ability2" : "eldritch invocations",
+    "ability3" : "pact boon"
   },
   {
-    "name": "Wizard"
+    "name": "Wizard",
+    "ability1" : "spell mastery",
+    "ability2" : "signature spells",
+    "ability3" : "hipnotic gaze"
   }
 ]
 const seedClass = () => Class.bulkCreate(classData);
